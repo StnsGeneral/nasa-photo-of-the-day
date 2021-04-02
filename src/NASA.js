597acbd0b838e8f68f2a531f9a2c20c79780163a
Tristan Miller <tristanmiller89@gmail.com>
2021-04-02 01:10:22 -0500

    finished mvp, played around, sorry it is so ugly

--- src/NASA.js
+++ src/NASA.js
@@ -1,9 +1,38 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+import styled from 'styled-components';
 
 const NasaData = () => {
   // Setting API data
   const [photoData, setPhotoData] = useState('');
+
+  // H1 styling
+  const StyledHeader = styled.h1`
+    color: red;
+    font-size: 3rem;
+    &:hover {
+      color: yellow;
+      background-color: blue;
+    }
+  `;
+
+  // Paragraph styling
+  const StyledExplanation = styled.p`
+    color: blue;
+    font-size: 1.5rem;
+    &:hover {
+      font-style: italic;
+    }
+  `;
+
+  // Image styling
+  const StyledImage = styled.img`
+    width: 60%;
+    margin: 10px;
+    &:hover {
+      transform: rotate(180deg);
+    }
+  `;
 
   // Effect call for API data
   useEffect(() => {
@@ -12,7 +41,7 @@ const NasaData = () => {
         'https://api.nasa.gov/planetary/apod?api_key=7o7Dg7vHLhSczxEwkDUpIff2UAbwzLuRGudxgKis'
       )
       .then((response) => {
-        // console.log(response.data); logged data to look at structure
+        console.log(response.data);
         setPhotoData(response.data);
       })
       .catch((error) => console.log(error));
@@ -21,12 +50,16 @@ const NasaData = () => {
   // Return containing the HTML structure
   return (
     <div className="container">
-      <h1 className="APOD date">NASA Photo of the day for {photoData.date}</h1>
-      <img
+      <StyledHeader className="APOD date">
+        NASA Photo of the day for {photoData.date}
+      </StyledHeader>
+      <StyledImage
         className="image-container"
         src={photoData.url}
-        alt={photoData.title}></img>
-      <p className="picture-explanation">{photoData.explanation}</p>
+        alt={photoData.title}></StyledImage>
+      <StyledExplanation className="picture-explanation">
+        {photoData.explanation}
+      </StyledExplanation>
     </div>
   );
 };
---
+++ src/themes/index.js
@@ -0,0 +1,9 @@
+// Themes for NASA APOD
+
+export default {
+  primaryTheme: {
+    primaryColor: 'red',
+    secondaryColor: 'blue',
+    tertiaryColor: 'green',
+  },
+};
